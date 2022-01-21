2f4ef9ce86ceee7f6bdd314c817823928336afd4
wuwenbang@foxmail.com <wuwenbang@foxmail.com>
2022-01-21 11:24:46 +0800

    Update deploy.sh

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -3,5 +3,7 @@ cd /home/winter/app/ &&
 git pull &&
 yarn install &&
 docker build -t winter/node-web-app . &&
-docker run --name app --network=host -p 3000:3000 -d winter/node-web-app
+docker kill app &&
+docker rm app &&
+docker run --name app --network=host -p 3000:3000 -d winter/node-web-app &&
 echo 'Deploy Successfully !'
